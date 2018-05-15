cec687a5df5b8df965dcdbaf59c208beb6b9bc46
Bartosz Lorek <bery.lorek@gmail.com>
2018-05-15 13:31:17 +0200

    create umd and minified (cjs) versions

--- rollup.config.js
+++ rollup.config.js
@@ -5,15 +5,31 @@ import babel from 'rollup-plugin-babel'
 
 export default {
     entry: 'src/index.js',
-    output: {
-        file: './dist/aframe.min.js',
-        format: 'umd',
-        name: 'aframe'
-    },
+    output: [
+        {
+            file: './dist/aframe.umd.js',
+            format: 'umd',
+            name: 'aframe'
+        },
+        {
+            file: './dist/aframe.min.js',
+            format: 'cjs'
+        }
+    ],
     plugins: [
         resolve(),
         commonjs(),
-        uglify(),
+        uglify({
+            compress: {
+                warnings: false,
+                drop_console: true,
+                drop_debugger: true,
+                reduce_funcs: false
+            },
+            mangle: {
+                toplevel: true
+            }
+        }),
         babel({
             exclude: ['node_modules/**']
         })
